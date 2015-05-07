d4c3d0ed413ce42f097d040cfee141c87b4dfe42
jazzyb <json.barnes@gmail.com>
2015-05-07 17:42:57 -0400

    bump version to 0.1.1
    adds OTP 17.5.3 to build sources

--- nectar/__init__.py
+++ nectar/__init__.py
@@ -31,6 +31,7 @@ class VersionManager (object):
             ('17.5',   'https://github.com/erlang/otp/archive/OTP-17.5.tar.gz'),
             ('17.5.1', 'https://github.com/erlang/otp/archive/OTP-17.5.1.tar.gz'),
             ('17.5.2', 'https://github.com/erlang/otp/archive/OTP-17.5.2.tar.gz'),
+            ('17.5.3', 'https://github.com/erlang/otp/archive/OTP-17.5.3.tar.gz'),
         ],
 
         'elixir': [
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='nectar',
-      version='0.1.0',
+      version='0.1.1',
       description='Yet another Elixir/OTP version manager',
       url='https://github.com/jazzyb/nectar',
       author='Jason M Barnes',
